c3ea210aad2672c64cc9e610d65a0228f7c831f3
tomicdusann <dusantomicroki@gmail.com>
2024-04-07 14:08:55 +0200

    Read functions implemented
    1. ReadInputRegistersFunction
    2. ReadHoldingRegistersFunction

--- dCom/Modbus/ModbusFunctions/ReadHoldingRegistersFunction.cs
+++ dCom/Modbus/ModbusFunctions/ReadHoldingRegistersFunction.cs
@@ -25,14 +25,47 @@ namespace Modbus.ModbusFunctions
         public override byte[] PackRequest()
         {
             //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+
+            ModbusReadCommandParameters parameters = CommandParameters as ModbusReadCommandParameters;
+
+            byte[] request = new byte[12];
+
+            request[0] = BitConverter.GetBytes(parameters.TransactionId)[1];
+            request[1] = BitConverter.GetBytes(parameters.TransactionId)[0];
+            request[2] = BitConverter.GetBytes(parameters.ProtocolId)[1];
+            request[3] = BitConverter.GetBytes(parameters.ProtocolId)[0];
+            request[4] = BitConverter.GetBytes(parameters.Length)[1];
+            request[5] = BitConverter.GetBytes(parameters.Length)[0];
+            request[6] = parameters.UnitId;
+            request[7] = parameters.FunctionCode;
+            request[8] = BitConverter.GetBytes(parameters.StartAddress)[1];
+            request[9] = BitConverter.GetBytes(parameters.StartAddress)[0];
+            request[10] = BitConverter.GetBytes(parameters.Quantity)[1];
+            request[11] = BitConverter.GetBytes(parameters.Quantity)[0];
+
+            return request;
         }
 
         /// <inheritdoc />
         public override Dictionary<Tuple<PointType, ushort>, ushort> ParseResponse(byte[] response)
         {
             //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+
+            Dictionary<Tuple<PointType, ushort>, ushort> responseDict = new Dictionary<Tuple<PointType, ushort>, ushort>();
+
+            int byteCounter = response[8];
+            ushort startAddress = ((ModbusReadCommandParameters)CommandParameters).StartAddress;
+
+            for (int i = 0; i < byteCounter; i += 2) { 
+            
+                ushort value = (ushort)IPAddress.NetworkToHostOrder((short)BitConverter.ToUInt16(response, 9 + i));
+                Tuple<PointType, ushort> tuple = new Tuple<PointType, ushort>(PointType.ANALOG_OUTPUT, startAddress++);
+                responseDict.Add(tuple, value);
+            }
+
+            return responseDict;
         }
     }
 }
--- dCom/Modbus/ModbusFunctions/ReadInputRegistersFunction.cs
+++ dCom/Modbus/ModbusFunctions/ReadInputRegistersFunction.cs
@@ -26,20 +26,52 @@ namespace Modbus.ModbusFunctions
         {
             Console.WriteLine("Request started");
             // ModbusReadCommandParameters nam treba
-            byte[] recVal = new byte[12];
+            //byte[] recVal = new byte[12];
+
+            ModbusReadCommandParameters parameters = CommandParameters as ModbusReadCommandParameters;
+
             //Head message
 
             // Data message
+            byte[] req = new byte[12];
 
-            return recVal;
-            Console.WriteLine("Request ended");
+            req[0] = BitConverter.GetBytes(parameters.TransactionId)[1];
+            req[1] = BitConverter.GetBytes(parameters.TransactionId)[0];
+            req[2] = BitConverter.GetBytes(parameters.ProtocolId)[1];
+            req[3] = BitConverter.GetBytes(parameters.ProtocolId)[0];
+            req[4] = BitConverter.GetBytes(parameters.Length)[1];
+            req[5] = BitConverter.GetBytes(parameters.Length)[0];
+            req[6] = parameters.UnitId;
+            req[7] = parameters.FunctionCode;
+            req[8] = BitConverter.GetBytes(parameters.StartAddress)[1];
+            req[9] = BitConverter.GetBytes(parameters.StartAddress)[0];
+            req[10] = BitConverter.GetBytes(parameters.Quantity)[1];
+            req[11] = BitConverter.GetBytes(parameters.Quantity)[0];
+
+            return req;
+
+            //Console.WriteLine("Request ended");
         }
 
         /// <inheritdoc />
         public override Dictionary<Tuple<PointType, ushort>, ushort> ParseResponse(byte[] response)
         {
             //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+
+            Dictionary<Tuple<PointType, ushort>, ushort> responseDict = new Dictionary<Tuple<PointType, ushort>, ushort>();
+
+            int byteCounter = response[8];
+            ushort startAddress = ((ModbusReadCommandParameters)CommandParameters).StartAddress;
+
+            for (int i = 0; i < byteCounter; i += 2) { 
+        
+                ushort value = (ushort)IPAddress.NetworkToHostOrder((short)BitConverter.ToUInt16(response, 9 + i));
+                Tuple<PointType, ushort> tuple = new Tuple<PointType, ushort>(PointType.ANALOG_INPUT, startAddress++);
+                responseDict.Add(tuple, value);
+            }
+
+            return responseDict;
         }
     }
 }
